656bdc6b063616dc4cc7021f0ef83ccdd271310e
Kidda0Snoogans <mx.astridburris@gmail.com>
2024-06-26 10:06:07 -0400

    balls

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-// JavaScript goes here.
+let Car_Question = document.querySelector(".Car_Question")
+let Bar_Question = document.querySelector(".Bar_Question")
+
+// For the first question //
+
+// For the flee button
+
+let Flee = document.querySelector(".Flee")
+
+Flee.addEventListener('click', function(){
+    Car_Question.style.display = "block";
+    Bar_Question.style.display = "none";
+    (let (x = 1));
+})
+
+// For the drink button
+
+let Drink = document.querySelector(".Drink")
+Drink.addEventListener('click', function(){
+    Car_Question.style.display = "block";
+    Bar_Question.style.display = "none"; 
+    (let (x = 2));
+})
+
+// For the fight button
+
+let Fight = document.querySelector(".Fight")
+Fight.addEventListener('click', function(){
+    Car_Question.style.display = "block";
+    Bar_Question.style.display = "none";
+    let (x = 3);
+})
+
+// For the Discuss button
+
+let Discuss = document.querySelector(".Discuss")
+Discuss.;{
+    Car_Question.style.display = "block";
+    Bar_Question.style.display = "none"; 
+    let (x = 4);
+});
+
+    //
+
+
+
+// For the results //
+
+let Goth_Results = document.querySelector(".Gothy")
+
+if ((x = 1) && (y = 1)) {
+    Gothy.style.display = "block";
+// } else {
+    
+}
